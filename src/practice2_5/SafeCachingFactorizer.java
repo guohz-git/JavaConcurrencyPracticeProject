a1f1dd3abe3942598d41d30fbe2550367a0eea04
天空只能仰望吗？ <351150710@qq.com>
2018-06-03 21:29:45 +0800

    Initial commit

---
+++ src/practice2_5/SafeCachingFactorizer.java
@@ -0,0 +1,31 @@
+package practice2_5;
+
+import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SafeCachingFactorizer {
+    private final AtomicReference<BigInteger> lastNamber
+            = new AtomicReference<>();
+
+    private final AtomicReference<BigInteger []> lastFactores
+            = new AtomicReference<>();
+
+    public  synchronized void service(BigInteger i){
+
+        if( i .equals(lastNamber.get())){
+            System.out.println(lastFactores.get());
+        }else{
+            BigInteger[] factors = new BigInteger[10];
+            factors[0] = i;
+
+            lastNamber.set(i);
+            lastFactores.set(factors);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        SafeCachingFactorizer obj = new SafeCachingFactorizer();
+        obj.service(BigInteger.valueOf(0L));
+    }
+}
---
+++ src/practice2_5/UnsafeCachingFactorizer.java
@@ -0,0 +1,31 @@
+package practice2_5;
+
+import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class UnsafeCachingFactorizer {
+    private final AtomicReference<BigInteger> lastNamber
+            = new AtomicReference<>();
+
+    private final AtomicReference<BigInteger []> lastFactores
+            = new AtomicReference<>();
+
+    public  void service(BigInteger i){
+
+        if( i .equals(lastNamber.get())){
+            System.out.println(lastFactores.get());
+        }else{
+            BigInteger[] factors = new BigInteger[10];
+            factors[0] = i;
+
+            lastNamber.set(i);
+            lastFactores.set(factors);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        UnsafeCachingFactorizer obj = new UnsafeCachingFactorizer();
+        obj.service(BigInteger.valueOf(0L));
+    }
+}
